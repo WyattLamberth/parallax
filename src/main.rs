8e8ee560f21f7516e28a7532ab6b198d428992c3
Wyatt Lamberth <wyattlamberth@outlook.com>
2025-12-28 18:28:43 -0600

    made rootfs path more portable using getcwd, and adding a return type to main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use nix::unistd;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let p = unistd::getpid();
-    let rootfs = "/Users/wyattlamberth/dev/parallax/rootfs/";
+    let cwd = unistd::getcwd()?;
+    let rootfs = cwd.join("rootfs");
     println!("Hello, world! my PID is {}", p);
-    match unistd::chroot(rootfs) {
-        Ok(_) => println!("Succesfully changed root filesystem to: {}", rootfs),
+    match unistd::chroot(&rootfs) {
+        Ok(_) => println!(
+            "Succesfully changed root filesystem to: {}",
+            rootfs.display()
+        ),
         Err(e) => println!(
             "Something went wrong attempting to chroot to fs: {}\n Error: {}",
-            rootfs, e
+            rootfs.display(),
+            e
         ),
     };
+    Ok(())
 }
